732e6ba2ccc8fb22eea5c80d1f4f1e0159f5f204
Rafał Mikrut <mikrutrafal@protonmail.com>
2023-04-16 21:09:20 +0200

    Renaming

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env::args;
 use std::path::Path;
 use std::process::Command;
@@ -5,6 +6,7 @@ use std::sync::atomic::{AtomicI32, Ordering};
 use std::{fs, process};
 
 use bk_tree::BKTree;
+use config::Config;
 use image_hasher::{HashAlg, HasherConfig};
 use rayon::prelude::*;
 use walkdir::WalkDir;
@@ -30,31 +32,48 @@ struct BasicInfo {
     possible_output_png_original: String,
 }
 
-fn main() {
-    let thorvg_path;
-    let path_to_files_to_check;
-    let size_of_file;
-    let similarity;
-    let mut ignore_conversion_step = false;
-    let mut ignore_with_text = false;
+struct Settings {
+    folder_with_files_to_check: String,
+    px_size_of_generated_file: u32,
+    ignore_conversion_step: bool,
+    ignore_with_text: bool,
+    similarity: u32,
+    output_folder: String,
 
-    let os_args: Vec<_> = args().collect();
-    if os_args.len() >= 5 {
-        thorvg_path = os_args[1].clone();
-        path_to_files_to_check = os_args[2].clone();
-        size_of_file = os_args[3].parse::<u32>().unwrap();
-        similarity = os_args[4].parse::<u32>().unwrap();
-        if os_args.len() >= 7 {
-            ignore_conversion_step = os_args[5].parse::<bool>().unwrap();
-            ignore_with_text = os_args[6].parse::<bool>().unwrap();
-        }
-    } else {
-        println!("You need to set 4 arguments - thorvg path, file with svg files to check(one per line), size of file(width is same as height), similarity(0 means very similar, bigger values check for less similar svgs)");
-        process::exit(1);
+    thorvg_path: String,
+    other_tool_path: String,
+}
+
+fn load_settings() -> Settings {
+    let settings = Config::builder()
+        .add_source(config::File::with_name("settings"))
+        .build()
+        .unwrap();
+    let config = settings
+        .try_deserialize::<HashMap<String, HashMap<String, String>>>()
+        .unwrap();
+
+    let general_settings = config["general"].clone();
+    let thorvg_settings = config["thorvg"].clone();
+    let other_tool_settings = config["other_tool"].clone();
+    Settings {
+        folder_with_files_to_check: general_settings["folder_with_files_to_check"].clone(),
+        px_size_of_generated_file: general_settings["px_size_of_generated_file"]
+            .parse()
+            .unwrap(),
+        ignore_conversion_step: general_settings["ignore_conversion_step"].parse().unwrap(),
+        ignore_with_text: general_settings["ignore_with_text"].parse().unwrap(),
+        similarity: general_settings["similarity"].parse().unwrap(),
+        output_folder: general_settings["output_folder"].clone(),
+        thorvg_path: thorvg_settings["path"].clone(),
+        other_tool_path: other_tool_settings["path"].clone(),
     }
+}
+
+fn find_files(settings: &Settings) -> Vec<String> {
     let mut files_to_check = Vec::new();
-    if Path::new(&path_to_files_to_check).is_dir() {
-        for entry in WalkDir::new(&path_to_files_to_check)
+    if Path::new(&settings.folder_with_files_to_check).is_dir() {
+        for entry in WalkDir::new(&settings.folder_with_files_to_check)
             .max_depth(1)
             .into_iter()
             .flatten()
@@ -72,7 +91,7 @@ fn main() {
             }
         }
     } else {
-        files_to_check = match fs::read_to_string(&path_to_files_to_check) {
+        files_to_check = match fs::read_to_string(&settings.folder_with_files_to_check) {
             Ok(t) => t
                 .split('\n')
                 .map(|e| e.trim())
@@ -82,13 +101,19 @@ fn main() {
             Err(e) => {
                 println!(
                     "Failed to open file {}, reason {}",
-                    path_to_files_to_check, e
+                    settings.folder_with_files_to_check, e
                 );
                 process::exit(1);
             }
         };
     }
+    files_to_check
+}
 
+fn main() {
+    let settings = load_settings();
+    let files_to_check = find_files(&settings);
+    let files_to_check = files_to_check[..100].to_vec(); // TODO remove it
     let atomic: AtomicI32 = AtomicI32::new(0);
 
     files_to_check.par_iter().for_each(|source_file| {
@@ -109,9 +134,9 @@ fn main() {
                     "-o".to_string(),
                     "OUTPUT_FILE".to_string(),
                     "-w".to_string(),
-                    size_of_file.to_string(),
+                    settings.px_size_of_generated_file.to_string(),
                     "-h".to_string(),
-                    size_of_file.to_string(),
+                    settings.px_size_of_generated_file.to_string(),
                 ],
             },
             // BasicInfo {
@@ -131,14 +156,17 @@ fn main() {
             // },
             BasicInfo {
                 name: "Thorvg".to_string(),
-                command: thorvg_path.clone(),
+                command: settings.thorvg_path.clone(),
                 output_png_added: "_thorvg.png",
                 possible_output_png_original: "".to_string(),
                 output_png: "".to_string(),
                 arguments: vec![
                     source_file.to_string(),
                     "-r".to_string(),
-                    format!("{}x{}", size_of_file, size_of_file),
+                    format!(
+                        "{}x{}",
+                        settings.px_size_of_generated_file, settings.px_size_of_generated_file
+                    ),
                 ],
             },
             // BasicInfo {
@@ -171,6 +199,20 @@ fn main() {
         ];
         assert_eq!(fields.len(), 2); // Only 2 are supported
 
+        if settings.ignore_with_text {
+            match fs::read_to_string(source_file) {
+                Ok(t) => {
+                    if t.contains("</text>") || t.contains("</filter>") {
+                        // println!("Ignoring {} with text", source_file);
+                        return;
+                    }
+                }
+                Err(_) => {
+                    return;
+                }
+            }
+        }
+
         for field in fields.iter_mut() {
             field.output_png = source_file.replace(".svg", field.output_png_added);
             field.possible_output_png_original = source_file.replace(".svg", ".png");
@@ -181,21 +223,7 @@ fn main() {
                 args.push(argument.replace("OUTPUT_FILE", &field.output_png))
             }
 
-            if ignore_with_text {
-                match fs::read_to_string(&source_file) {
-                    Ok(t) => {
-                        if t.contains("</text>") || t.contains("</filter>") {
-                            // println!("Ignoring {} with text", source_file);
-                            return;
-                        }
-                    }
-                    Err(_) => {
-                        return;
-                    }
-                }
-            }
-
-            if !ignore_conversion_step {
+            if !settings.ignore_conversion_step {
                 // Run command
                 let _output = Command::new(&field.command).args(args).output().unwrap();
 
@@ -268,7 +296,7 @@ fn main() {
             None => 999999,
         };
 
-        if !finds.is_empty() && similarity_found <= similarity {
+        if !finds.is_empty() && similarity_found <= settings.similarity {
             // println!(
             //     "VALID conversion, {} and {} have same output for {}",
             //     fields[0].name, fields[1].name, source_file
@@ -282,6 +310,45 @@ fn main() {
                 "\tfirefox {}; firefox {}; firefox {}",
                 source_file, fields[0].output_png, fields[1].output_png
             ); // I found that the best to compare images, is to open them in firefox and switch tabs,
+            fs::copy(
+                &fields[0].output_png,
+                format!(
+                    "{}/{}",
+                    settings.output_folder,
+                    Path::new(&fields[0].output_png)
+                        .file_name()
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                ),
+            )
+            .unwrap();
+            fs::copy(
+                &fields[1].output_png,
+                format!(
+                    "{}/{}",
+                    settings.output_folder,
+                    Path::new(&fields[1].output_png)
+                        .file_name()
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                ),
+            )
+            .unwrap();
+            fs::copy(
+                &source_file,
+                format!(
+                    "{}/{}",
+                    settings.output_folder,
+                    Path::new(&source_file)
+                        .file_name()
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                ),
+            )
+            .unwrap();
             println!();
         }
     });
